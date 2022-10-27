fdb151162d8d445b216ad2249d96ec67f40a0299
apeli23 <apelibrian@gmail.com>
2022-10-27 18:34:55 +0300

    bug-fix+demo

--- components/LayoutWrapper.js
+++ components/LayoutWrapper.js
@@ -33,7 +33,7 @@ const LayoutWrapper = ({ children }) => {
   }, [ref])
 
   return (
-    <>
+    <div>
       <header className={classes} ref={ref}>
         <div className="mx-auto flex max-w-3xl items-center justify-between bg-cardBg bg-opacity-5 px-4 sm:px-6 xl:max-w-5xl xl:px-0">
           <Link href="/" aria-label="Tech Blog">
@@ -107,7 +107,7 @@ const LayoutWrapper = ({ children }) => {
         <main className="mb-auto">{children}</main>
         <Footer />
       </SectionContainer>
-    </>
+    </div>
   )
 }
 
